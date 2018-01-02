eaf8a3588ae547e8a7e9314d15f3d42ecb83c40f
ruudi170 <ruudi170@gmail.com>
2018-01-02 12:14:04 +0200

    local.template

--- local.template.js
+++ local.template.js
@@ -1,4 +1,14 @@
+var mode = 'production';
+
+if (!process.env.mode || process.env.mode === 'dev' || process.env.mode === 'development') {
+  mode = 'development';
+}
+
 module.exports = {
   port: '20000',
-  host: '0.0.0.0'
+  host: '0.0.0.0',
+  mode: mode,
+  mongo: {
+    uri: 'mongodb://192.168.0.2:27017/20000'
+  }
 };
